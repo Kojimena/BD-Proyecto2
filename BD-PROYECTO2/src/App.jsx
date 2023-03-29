5c6e15e19c3d702f489074789f5295397c404a0d
TheKiesling <kie21581@uvg.edu.gt>
2023-03-29 16:48:18 -0600

    Update App.jsx

--- BD-PROYECTO2/src/App.jsx
+++ BD-PROYECTO2/src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Navigation from "./Pages/Navigation";
 import './App.css';
+import { Redirect } from "react-router-dom";
 
-function App() {
+function App({username}) {
 
+    const [login, setLogin] = useState(false)
+
+    return(
+        <form load = {() => login ?  Navigation() : Login() }>
+        
+        </form>
+    )
 }
 
+function Login(){
+    return <h2>Login</h2>
+}
 
 export default App
