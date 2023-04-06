503aff9709fa3cba40eb1446a7451aa0698229b9
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-04-06 15:07:04 -0600

    changes

--- BD-PROYECTO2/src/App.jsx
+++ BD-PROYECTO2/src/App.jsx
@@ -1,21 +1,107 @@
-import React from "react";
-import Navigation from "./Pages/Navigation";
+import React, {useState} from "react";
 import './App.css';
-import { Redirect } from "react-router-dom";
+import { 
+  Redirect,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link 
+  } from "react-router-dom";
 
-function App({username}) {
 
-    const [login, setLogin] = useState(false)
+function App() {
+
+    const [login, setLogin] = useState(true)
+
+    const logged = () => {
+      // Aquí deberías verificar si las credenciales del usuario son correctas
+      // y establecer el estado de autenticación en consecuencia
+      setLogin(true);
+    };
+
+    const logout = () => {
+
+      // Aquí deberías hacer el logout del usuario
+      setLogin(false);
+    };
+
+    const redirectPath = login ? "/navigation" : "/login";
 
     return(
-        <form load = {() => login ?  Navigation() : Login() }>
-        
-        </form>
+      <Router>
+        <div> 
+          <Switch>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/navigation">
+              <Navigation />
+            </Route>
+            <Route path="/">
+              <Redirect to={redirectPath} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     )
 }
 
 function Login(){
     return <h2>Login</h2>
+}
+
+function Home() {
+  return <h2>INFO USER</h2>;
+}
+
+function About() {
+  return <h2>INVENTORY</h2>;
+}
+
+function Users() {
+  return <h2>SEARCH RECORD</h2>;
+}
+
+
+
+function Navigation(){
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Account</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">Search inventory</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/users" className="nav-link">Search record</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">Log out</Link>
+            </li>
+          </ul>
+        </nav>
+  
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
