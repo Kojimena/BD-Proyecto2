65bc84b325d27ad2075e2e2d685c70ba41c84dac
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-04-16 11:42:14 -0600

    erase backend old

--- BD-PROYECTO2/backend/routes/healthUnit.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getHealthUnit, postHealthUnit } from '../controllers/HealthUnit.js';
-
-const router = express.Router();
-
-router.get("/", getHealthUnit)
-router.post("/posttesthu", postHealthUnit)
-
-export default router
