9922c52f4642b2d1b4507175e6ba2df5ae6e1784
pabloozamora <pabloozamora02@gmail.com>
2023-04-21 15:32:57 -0600

    Corrección API

--- BD-PROYECTO2/frontend/src/api.js
+++ BD-PROYECTO2/frontend/src/api.js
@@ -1,2 +1,2 @@
-//export const API_URL = 'http://3.101.148.58'
-export const API_URL = 'https://medgt.uvgenios.online'
+export const API_URL = 'http://3.101.148.58'
+//export const API_URL = 'https://medgt.uvgenios.online'
