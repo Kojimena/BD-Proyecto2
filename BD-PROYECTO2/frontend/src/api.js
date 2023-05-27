051d0e69d110134128fa0378e8369c658eee23b1
Mark Albrand <mark.albrand5@gmail.com>
2023-05-27 12:52:23 -0600

    Update api.js

--- BD-PROYECTO2/frontend/src/api.js
+++ BD-PROYECTO2/frontend/src/api.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://3.101.148.58'
+//export const API_URL = 'http://3.101.148.58'
 //export const API_URL = 'https://medgt.uvgenios.online'
+export const API_URL = 'http://ec2-13-57-35-73.us-west-1.compute.amazonaws.com'
